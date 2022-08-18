a534653da3332165708809f630700bfeecbe7263
Atharva Pardeshi <atharva.exe@gmail.com>
2022-08-18 11:04:47 +0530

    feat: write directory component and its styles from the homepage prototype

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+
+import "./directory.styles.scss";
+
+import MenuItem from "../menu-item/menu-item.component";
+
+// this will be a class component because
+// we want to use state for the menu-items
+class Directory extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      sections: [
+        {
+          title: "hats",
+          imageUrl:
+            "https://images.unsplash.com/photo-1534215754734-18e55d13e346?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=704&q=80",
+          credit: "Ahmed Syed",
+          id: 1,
+        },
+        {
+          title: "jackets",
+          imageUrl:
+            "https://images.unsplash.com/photo-1559551409-dadc959f76b8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1473&q=80",
+          credit: "Tessa Simpson",
+          id: 2,
+        },
+        {
+          title: "sneakers",
+          imageUrl:
+            "https://images.unsplash.com/photo-1595950653106-6c9ebd614d3a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+          credit: "Ryan Plomp",
+          id: 3,
+        },
+        {
+          title: "women",
+          imageUrl:
+            "https://images.unsplash.com/photo-1581404917879-53e19259fdda?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469&q=80",
+          credit: "Adam Winger",
+          id: 4,
+        },
+        {
+          title: "men",
+          imageUrl:
+            "https://images.unsplash.com/photo-1496345875659-11f7dd282d1d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+          credit: "Brooke Cagle",
+          id: 5,
+        },
+        ,
+      ],
+    };
+  }
+  render() {
+    return (
+      <div className="directory-menu">
+        {this.state.sections.map(({ title, id, imageUrl }) => (
+          <MenuItem key={id} title={title} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Directory;
